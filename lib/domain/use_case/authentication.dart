8ce2916df9af733a163e7835b47d30d8abb8fcb7
Alefig12 <caguanae@uninorte.edu.com>
2022-09-21 12:27:45 -0500

    changing authentication

--- lib/domain/use_case/authentication.dart
+++ lib/domain/use_case/authentication.dart
@@ -11,8 +11,20 @@ class Authentication {
       await _sharedPreferences.retrieveData<bool>('logged') ?? false;
 
   Future<bool> login(user, password) async {
+
+    final loggedUser = await _sharedPreferences.retrieveData<String>('user');
+    final loggedPassword = await _sharedPreferences.retrieveData<String>('password');
+
+    if (user == loggedUser && password == loggedPassword) {
+      await _sharedPreferences.storeData('logged', true);
+      return Future.value(true);
+    }
+    return Future.value(false);
+
     // verificar si user y password coinciden con los almacenados
     // en ese caso cambiar el estado de loggeed y devolver  Future.value(true);
+
+    
   }
 
   Future<void> signup(user, password) async {
